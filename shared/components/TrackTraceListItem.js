841d8af30d9f2dc7eaba35a59e4fac578edeab3b
Timon Snetselaar <timonsn@gmail.com>
2015-10-30 11:33:14 +0100

    Adds images for status of the shipments

--- shared/components/TrackTraceListItem.js
+++ shared/components/TrackTraceListItem.js
@@ -1,10 +1,24 @@
 import React from 'react-native';
-const {StyleSheet, Text, View} = React;
+const {StyleSheet, Text, View, Image} = React;
 
 class TrackTraceListItem extends React.Component {
   render() {
     const {shipment: {consignmentNumber, deliveryTown}} = this.props;
-    return <View><Text>{consignmentNumber}</Text><Text>{deliveryTown}</Text></View>;
+
+    var icon = {
+        'COLING' : require('image!COLING'),
+        'COLTED' : require('image!COLTED'),
+        'DELING' : require('image!DELING'),
+        'DELRED' : require('image!DELRED'),
+        'INTRAN' : require('image!INTRAN'),
+    }[this.props.shipment.statusData[0].groupCode];
+
+    return <View>
+            <Image source={icon}
+                   style={{width: 300, height: 300}} />
+            <Text>{consignmentNumber}</Text>
+            <Text>{deliveryTown}</Text>
+           </View>;
   }
 }
 
