67fdf3daa47c86261a5a1ec254b583be805110b8
Ruben Oostinga <roostinga@xebia.com>
2015-10-30 12:58:09 +0100

    Make a Button component

--- shared/components/TrackTraceListItem.js
+++ shared/components/TrackTraceListItem.js
@@ -1,21 +1,18 @@
 import React from 'react-native';
 const {StyleSheet, Text, View, Image} = React;
-import  {MKButton} from 'react-native-material-kit';
 
-const ColoredRaisedButton = MKButton.coloredButton()
-  .withText('BUTTON')
-  .build();
+import Button from './Button.js';
 
 class TrackTraceListItem extends React.Component {
   render() {
     const {shipment: {consignmentNumber, deliveryTown}} = this.props;
 
     var icon = {
-        'COLING' : require('image!coling'),
-        'COLTED' : require('image!colted'),
-        'DELING' : require('image!deling'),
-        'DELRED' : require('image!delred'),
-        'INTRAN' : require('image!intran'),
+      'COLING': require('image!coling'),
+      'COLTED': require('image!colted'),
+      'DELING': require('image!deling'),
+      'DELRED': require('image!delred'),
+      'INTRAN': require('image!intran'),
     }[this.props.shipment.statusData[0].groupCode];
 
     return <View>
@@ -23,7 +20,7 @@ class TrackTraceListItem extends React.Component {
              style={{width: 300, height: 300}}/>
       <Text>{consignmentNumber}</Text>
       <Text>{deliveryTown}</Text>
-      <ColoredRaisedButton/>
+      <Button text="Details"/>
     </View>;
   }
 }
